1e88d76e5018b5d18d87bfe043fb167138f958bc
Rehaan Rafiq <rehaanrafiq4@gmail.com>
2023-11-13 16:46:01 +0000

    Create Question model

---
+++ app/Models/Question.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Question extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title'. 'body'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value) {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+}
---
+++ database/migrations/2023_11_13_154010_create_questions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            Schema::dropIfExists('questions');
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('body');
+            $table->unsignedInteger('views')->default(0);
+            $table->unsignedInteger('answers')->default(0);
+            $table->integer('votes')->default(0);
+            $table->unsignedInteger('best_answer_id')->nullable();
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
