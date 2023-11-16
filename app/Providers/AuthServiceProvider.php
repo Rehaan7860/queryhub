0f5fb34a2579b9bb4c179e6147eabec05fc58eb3
Rehaan Rafiq <rehaanrafiq4@gmail.com>
2023-11-16 18:54:54 +0000

    Add authorization to prevent users editing/deleting others questions

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +22,14 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
+        Gate::define('update-question', function ($user, $question) {
+            return $user->id === $question->user_id;
+        });
+
+        Gate::define('delete-question', function ($user, $question) {
+            return $user->id === $question->user_id;
+        });
     }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -37,12 +37,18 @@
                                             </a>
                                         </h3>
                                         <div class="ml-auto d-flex gap-2">
+
+                                            @if(Auth::user()->can('update-question', $question))
                                             <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                            @endif
+
+                                                @if(Auth::user()->can('delete-question', $question))
                                             <form action="{{ route('questions.destroy', $question->id) }}" method="POST">
                                                 @method('DELETE')
                                                 @csrf
                                                 <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure you want to delete this question?')">Delete</button>
                                             </form>
+                                                @endif
                                         </div>
                                     </div>
 
