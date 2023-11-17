0ad3ab7ce291ee192975ad57f428f1e9743e0b27
Rehaan Rafiq <rehaanrafiq4@gmail.com>
2023-11-17 18:26:42 +0000

    Create answers tables for questions

---
+++ database/factories/AnswerFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Answer>
+ */
+class AnswerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'body' => $this->faker->paragraph(rand(3 , 7 ), true),
+            'user_id' => User::pluck('id')->random(),
+            'votes_count' => rand(0, 5),
+
+        ];
+    }
+}
